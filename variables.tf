babca569178c5c5a2e12bf9c3e8bb85285b4b518
Rahul Sharma <163303836+rahulSh83@users.noreply.github.com>
2025-04-04 15:42:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,23 @@
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
 variable "resource_group_name" {
   default = "myGitDeployRG"
 }
