d2eb6fe9a9dd71468c3b31c3c5ab301525ecac34
Rahul Sharma <163303836+rahulSh83@users.noreply.github.com>
2025-04-04 15:46:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,42 @@
 provider "azurerm" {
   features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
+# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_app_service_plan" "app_service_plan" {
+# App Service Plan
+resource "azurerm_app_service_plan" "plan" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Basic"
+    size = "B1"
   }
 }
 
-resource "azurerm_app_service" "app_service" {
+# App Service (Web App)
+resource "azurerm_app_service" "app" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+  app_service_plan_id = azurerm_app_service_plan.plan.id
 
   site_config {
-    scm_type = "GitHub"
+    always_on = true
   }
 
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "0"
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
-
-  lifecycle {
-    ignore_changes = [
-      site_config[0].scm_type # to avoid recreation if GitHub link changes externally
-    ]
-  }
-}
-
-resource "azurerm_app_service_source_control" "source_control" {
-  app_id     = azurerm_app_service.app_service.id
-  repo_url   = var.repo_url
-  branch     = var.branch
-  use_manual_integration = true
-  use_mercurial = false
 }
